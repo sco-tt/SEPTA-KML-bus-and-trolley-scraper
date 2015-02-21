16e1be0926d7bb3ed27024034ead091dcb10ad10
sco_tt <scott@scottpinkelman.com>
2015-02-21 18:01:06 -0500

    package.json, working on readme

---
+++ README.md
@@ -0,0 +1,40 @@
+Dependencies
+
+NodeJS
+npm
+Python 2.7
+Python Libraries:
+	BeautifulSoup                                                   
+	urllib2
+	requests
+	shutil
+	os
+	subprocess
+
+
+Usage:
+
+npm install
+chmod +x scrape-septa-kml.py
+python scrape-septa-kml.py
+
+This will create two directories, 'kml' and 'geojson' that include all routes of the respective file type. Filename is [route]-[type].[extension], like 13-trolley.kml or 33-bus.geojson.
+
+If you'd like to compress `all_routes.geojson`, install geojson-minifier globally.
+
+npm install -g geojson-minifier
+geojson-minifier -o pack -f all_routes.geojson -p 6
+
+That will compress all_routes.geojson to ~10 MB. It's possible to load with leaflet.js if you change the extension to .js and declare a variable in the first line, like:
+
+var septa = {
+  "type": "FeatureCollection",
+  "features": [
+    {
+      "type": "Feature",
+
+and so on.
+
+
+
+
---
+++ scrape-septa-kml.py
@@ -0,0 +1,70 @@
+from bs4 import BeautifulSoup                                                   
+import urllib2
+import requests
+import shutil
+import os
+import subprocess
+
+# Make Project Directories and assign variables
+projectdir = os.getcwd()
+os.mkdir(os.path.join(projectdir, 'kml'))
+kml_dir = os.path.join(projectdir, 'kml')
+os.mkdir(os.path.join(projectdir, 'geojson'))
+geojson_dir = os.path.join(projectdir, 'geojson')
+
+# scrape data from SEPTA's website to get all of the routes
+buses = BeautifulSoup(urllib2.urlopen('http://www.septa.org/schedules/bus/index.html').read())
+bus_matches = buses.find_all("div", class_="route_num")
+trolleys = BeautifulSoup(urllib2.urlopen('http://www.septa.org/schedules/trolley/index.html').read())
+trolley_matches = trolleys.find_all("div", class_="route_num")
+
+print "Downloading bus route KML files"
+for x in bus_matches:
+    if x.string:
+		route = x.string.strip().upper() 		# Must be uppercase or route 47M is going to fail			
+		url = 'http://www3.septa.org/transitview/kml/%s.kml' % route
+		response = requests.get(url, stream=True)
+		if response.status_code == 200:
+			with open('%s/%s-bus.kml' % (kml_dir,route), 'wb') as out_file:
+			    shutil.copyfileobj(response.raw, out_file)
+			print "Sucessfully downloaded from: %s" % url
+			del response
+		else:
+			print "Downloaded from %s failed" % url
+
+print "Downloading trolley route KML files"
+for x in trolley_matches:
+    if x.string:
+		route = x.string.strip().upper() 				
+		url = 'http://www3.septa.org/transitview/kml/%s.kml' % route
+		response = requests.get(url, stream=True)
+		if response.status_code == 200:
+			with open('%s/%s-trolley.kml' % (kml_dir,route), 'wb') as out_file:
+			    shutil.copyfileobj(response.raw, out_file)
+			print "Sucessfully downloaded from: %s" % url
+			del response
+		else:
+			print "Downloaded from %s failed" % url
+
+print "Converting to geoJSON"
+# Define node module utilities
+togeojson_util = os.path.join(projectdir, 'node_modules/togeojson')
+geojson_merge_util = os.path.join(projectdir, 'node_modules/geojson-merge')
+
+for filename in os.listdir(kml_dir):
+	filepath = os.path.join(kml_dir, filename)
+	route_name = os.path.splitext(filename)[0]
+	subprocess.call(["%s/togeojson %s > %s/%s.geojson" % (togeojson_util, filepath, geojson_dir, route_name)], shell=True)
+	print "%s convereted to geoJSON" % route_name
+	
+print "Merging geojson files"
+all_geojson_files = ""
+for filename in os.listdir(geojson_dir):
+	filepath = os.path.join(geojson_dir, filename)
+	all_geojson_files += "geojson/%s " % filename
+subprocess.call(["%s/geojson-merge %s > all_routes.geojson" % (geojson_merge_util, all_geojson_files)], shell=True)
+
+
+
+
+
