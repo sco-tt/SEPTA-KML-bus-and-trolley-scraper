468ce31ac717f9064789fdbca13a91a5250b3d9b
sco_tt <scott@scottpinkelman.com>
2015-02-21 10:14:48 -0500

    initial commit

---
+++ scraper.py
@@ -0,0 +1,46 @@
+from bs4 import BeautifulSoup                                                   
+import urllib2
+# https://stackoverflow.com/questions/13137817/how-to-download-image-using-requests
+# http://python.dzone.com/articles/how-download-file-python
+import requests
+import shutil
+
+buses = BeautifulSoup(urllib2.urlopen('http://www.septa.org/schedules/bus/index.html').read())
+bus_matches = buses.find_all("div", class_="route_num")
+trolleys = BeautifulSoup(urllib2.urlopen('http://www.septa.org/schedules/trolley/index.html').read())
+trolley_matches = trolleys.find_all("div", class_="route_num")
+
+print "Download bus route KML files"
+
+for x in bus_matches:
+    if x.string:
+		route = x.string.strip().upper() 				#Must be uppercase or route 47M is going to fail
+		url = 'http://www3.septa.org/transitview/kml/%s.kml' % route
+		response = requests.get(url, stream=True)
+		if response.status_code == 200:
+			with open('%s-bus.kml' % route, 'wb') as out_file:
+			    shutil.copyfileobj(response.raw, out_file)
+			print "Sucessfully downloaded from: %s" % url
+			del response
+		else:
+			print "Downloaded from %s failed" % url
+
+print "Download trolley route KML files"
+
+for x in trolley_matches:
+    if x.string:
+		route = x.string.strip().upper() 				
+		url = 'http://www3.septa.org/transitview/kml/%s.kml' % route
+		response = requests.get(url, stream=True)
+		if response.status_code == 200:
+			with open('%s-trolley.kml' % route, 'wb') as out_file:
+			    shutil.copyfileobj(response.raw, out_file)
+			print "Sucessfully downloaded from: %s" % url
+			del response
+		else:
+			print "Downloaded from %s failed" % url
+
+
+
+
+
